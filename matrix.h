16a2f1efd2d584263acc8d11c99d6ae6c4254eb5
SosninRoman <sosninroman92@gmail.com>
2018-11-11 00:15:16 +0500

    Доработки

--- matrix.h
+++ matrix.h
@@ -1,7 +1,6 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-#include <cstdlib>
 #include <unordered_map>
 #include <memory>
 
@@ -17,9 +16,28 @@ template<class T, size_t D>
 struct Node
 {
     Node() = default;
-    Node(size_t ind, Node<T,D+1>* parent = nullptr):ind(ind),parent(parent){}
-    size_t ind;
-    std::unordered_map<size_t, std::unique_ptr<Node<T, D-1>> > children;
+    explicit Node(size_t ind, Node<T,D+1>* parent = nullptr):ind(ind), parent(parent){}
+
+    Node(const Node& rhs, Node<T,D+1>* parent = nullptr):
+        ind(ind), parent(parent)
+    {
+        for(const auto& child : rhs.children)
+        {
+            auto node = std::make_shared<Node<T, D-1>>(&child.second, this);
+            children.emplace(child.first, std::move(node) );
+        }
+    }
+
+    size_t size() const
+    {
+        size_t result = 0;
+        for(const auto& child : children)
+            result += child.second->size();
+        return result;
+    }
+
+    size_t ind = 0;
+    std::unordered_map<size_t, std::shared_ptr<Node<T, D-1>> > children;
     Node<T,D+1>* parent = nullptr;
 };
 
@@ -27,8 +45,17 @@ template<class T>
 struct Node<T, 0>
 {
     Node() = default;
-    Node(size_t ind, Node<T,1>* parent = nullptr):ind(ind),parent(parent){}
+    explicit Node(Node<T,1>* parent = nullptr): parent(parent) {}
+
+    Node(size_t ind, Node<T,1>* parent = nullptr): parent(parent), ind(ind){}
+
+    Node(size_t ind, const Node& rhs, Node<T, 1>* parent = nullptr):
+        ind(ind), value(rhs.value), parent(parent){}
+
     Node(const T& val):value(val){}
+
+    size_t size() const {return 1;}
+
     size_t ind = 0;
     T value;
     Node<T,1>* parent = nullptr;
@@ -37,58 +64,127 @@ struct Node<T, 0>
 template<class T, size_t D>
 using NodeSh = std::shared_ptr<internal::Node<T,D>>;
 
+//template<class T, size_t D>
+//struct MatrixIterator
+//{
+//    typedef std::bidirectional_iterator_tag iterator_category;
+
+//    MatrixIterator() = default;
+
+//    explicit MatrixIterator(Node<* node) noexcept
+//        : node(node) { }
+
+//    T& operator*() const noexcept
+//    {
+//        return static_cast<Node<T>*>(node)->value;
+//    }
+
+//    T* operator->() const noexcept
+//    {
+//        return &(static_cast<Node<T>*>(node)->value);
+//    }
+
+//    SListIterator& operator++() noexcept
+//    {
+//        node = node->next;
+//        return *this;
+//    }
+
+//    SListIterator operator++(int) noexcept
+//    {
+//        SListIterator tmp = *this;
+//        node = node->next;
+//        return tmp;
+//    }
+
+//    bool operator==(const SListIterator& rhs) const noexcept
+//    { return node == rhs.node; }
+
+//    bool operator!=(const SListIterator& rhs) const noexcept
+//    { return node != rhs.node; }
+
+//    BaseNode* node;
+//};
+
 }
 
 template<class T, size_t D, T DefaultValue = T()>
 class Matrix
 {    
 public:
-    Matrix() = default;
+    Matrix()
+    {
+        m_node = std::make_shared<internal::Node<T,D>>();
+    }
+
+    Matrix(const Matrix& rhs)
+    {
+        for(const auto& node : rhs.m_node->children)
+            m_node->children.emplace(node.first,
+                            std::make_shared<internal::Node<T,D-1>>(*node.second) );
+    }
+
     Matrix<T,D-1, DefaultValue> operator[](size_t ind)
     {
-        auto itr = m_nodes.find(ind);
-        if(itr != m_nodes.end() )
+        auto itr = m_node->children.find(ind);
+        if(itr != m_node->children.end() )
            return Matrix<T,D-1,DefaultValue>(itr->second);
         else
         {
-            m_nodes.emplace(ind, std::make_shared<internal::Node<T,D-1>>() );
-            return Matrix<T, D-1, DefaultValue>(m_nodes[ind]);
+            m_node->children.emplace(ind, std::make_shared<internal::Node<T,D-1>>(ind, m_node.get() ) );
+            return Matrix<T, D-1, DefaultValue>(m_node->children[ind]);
         }
     }
+
+    size_t size() const
+    {
+        return m_node->size();
+    }
+
     template<class T1, std::size_t D1, T1 DefaultValue1> friend class Matrix;
 private:
-    Matrix(const internal::NodeSh<T,D>& node)
-    {
-        for(const auto& child : node->children)
-            m_nodes.emplace(child->ind, child);
-    }
-    std::unordered_map<size_t, internal::NodeSh<T,D-1>> m_nodes;
+    Matrix(internal::NodeSh<T,D> node): m_node(std::move(node) ){}
+
+    internal::NodeSh<T,D> m_node;
 };
 
 template<class T, T DefaultValue>
-class Matrix<T, 1, DefaultValue>
+class Matrix<T,1,DefaultValue>
 {
 public:
-    Matrix() = default;
-    Matrix<T, 0, DefaultValue> operator[](size_t ind)
+    Matrix()
     {
-        auto itr = m_nodes.find(ind);
-        if(itr != m_nodes.end() )
-           return Matrix<T, 0, DefaultValue>(itr->second, this);
+        m_node = std::make_shared<internal::Node<T,1>>();
+    }
+
+    Matrix(const Matrix& rhs)
+    {
+        for(const auto& node : rhs.m_node->children)
+            m_node->children.emplace(node.first,
+                            std::make_shared<internal::Node<T,0>>(*node.second) );
+    }
+
+    Matrix<T,0, DefaultValue> operator[](size_t ind)
+    {
+        auto itr = m_node->children.find(ind);
+        if(itr != m_node->children.end() )
+           return Matrix<T,0,DefaultValue>(itr->second);
         else
         {
-            m_nodes.emplace(ind, std::make_shared<internal::Node<T, 0>>() );
-            return Matrix<T, 0, DefaultValue>(m_nodes[ind], this);
+            return Matrix<T,0,DefaultValue>(m_node.get(), ind);
         }
     }
+
+    size_t size() const
+    {
+        return m_node->size();
+    }
+
     template<class T1, std::size_t D1, T1 DefaultValue1> friend class Matrix;
 private:
-    Matrix(const internal::NodeSh<T, 1>& node)
-    {
-        for(const auto& child : node->children)
-            m_nodes.emplace(child->ind, child);
-    }
-    std::unordered_map<size_t, internal::NodeSh<T, 0>> m_nodes;
+    Matrix(internal::NodeSh<T,1> node): m_node(std::move(node) ){}
+
+    internal::NodeSh<T,1> m_node;
 };
 
 template<class T, T DefaultValue>
@@ -96,18 +192,20 @@ class Matrix<T, 0, DefaultValue>
 {
     using NodeSh = std::shared_ptr<internal::Node<T,0>>;
 public:
-    Matrix() {}
+    Matrix() = default;
 
     Matrix(const T& v)
     {
         m_node = std::make_shared<internal::Node<T,0>>(v);
     }
 
-    template<class U, size_t D, T Defa>
-    Matrix(const Matrix<U,D,Defa>& rhs)
+    template<class U, size_t D, T Default>
+    Matrix(const Matrix<U,D,Default>& rhs)
     {
         if(rhs.m_node)
             m_node = std::make_shared<internal::Node<T,0>>(rhs.m_node->value);
+        else
+            m_node = std::make_shared<internal::Node<T,0>>(Default);
     }
 
     Matrix(const Matrix& rhs)
@@ -116,36 +214,54 @@ public:
             m_node = std::make_shared<internal::Node<T,0>>(rhs.m_node->value);
     }
 
-    Matrix(Matrix&& ppp)
-    {
-        int a = 1;
-    }
-
     ~Matrix()
     {
-        if(parent && (!m_node || m_node->value == DefaultValue) )
+        if(m_node)
         {
-            auto itr = parent->m_nodes.find(m_node->ind);
-            if(itr != parent->m_nodes.end() )
-                parent->m_nodes.erase(itr);
+            auto parent = m_node->parent;
+            if(parent && (m_node->value == DefaultValue) )
+            {
+                auto itr = parent->children.find(m_node->ind);
+                if(itr != parent->children.end() )
+                    parent->children.erase(itr);
+
+            }
         }
     }
-    operator T(){return m_node ? m_node->value : DefaultValue;}
+
+    size_t size() const {return 1;}
+
     operator T() const {return m_node ? m_node->value : DefaultValue;}
-    Matrix& operator=(const T& v)
+
+    template<class U>
+    Matrix& operator=(const U& v)
     {
         if(!m_node)
+        {
             m_node = std::make_shared<internal::Node<T,0>>(v);
-        m_node->value = v;
+            if(m_parent)
+            {
+                m_node->parent = m_parent;
+                m_node->ind = m_ind;
+                m_node->parent->children.emplace(m_ind, m_node);
+            }
+        }
+        else
+            m_node->value = v;
         return *this;
     }
+
     template<class T1, std::size_t D1, T1 DefaultValue1> friend class Matrix;
 private:
-    Matrix(NodeSh node, Matrix<T, 1, DefaultValue>* parent):
-        m_node(std::move(node) ), parent(parent)
+    Matrix(internal::Node<T,1>* parent, size_t ind):m_ind(ind), m_parent(parent){}
+
+    Matrix(NodeSh node):
+        m_parent(node->parent), m_node(std::move(node) )
     {}
+
+    size_t m_ind = 0;
+    internal::Node<T,1>* m_parent = nullptr;
     NodeSh m_node = nullptr;
-    Matrix<T, 1, DefaultValue>* parent = nullptr;
 };
 
 }
--- test/matrixtest.cpp
+++ test/matrixtest.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 #include "matrix.h"
+#include <vector>
+#include <map>
 
 TEST(MATRIX_TEST, zero_dimentional_matrix_test)
 {
@@ -8,13 +10,15 @@ TEST(MATRIX_TEST, zero_dimentional_matrix_test)
     zdm = 5;
     ASSERT_EQ(zdm, 5);
 
-    const matrix::Matrix<int,0,3> czdm;
-    ASSERT_EQ(czdm, 3);
+    const matrix::Matrix<int,0,-1> czdm;
+    ASSERT_EQ(czdm, -1);
     //czdm = 1;
-    zdm = 3;
+    zdm = -1;
     ASSERT_EQ(zdm, czdm);
+    zdm = 3;
+    ASSERT_EQ(zdm, 3);
 
-    const matrix::Matrix<int, 0, 3> czdm2;
+    const matrix::Matrix<int, 0, -1> czdm2;
     const matrix::Matrix<int, 0, 4> czdm3;
     ASSERT_EQ(czdm, czdm2);
     ASSERT_NE(czdm, czdm3);
@@ -24,6 +28,26 @@ TEST(MATRIX_TEST, zero_dimentional_matrix_test)
 
     matrix::Matrix<int,0> zdm3 = 9;
     ASSERT_EQ(zdm3,9);
+
+    ASSERT_EQ(zdm3.size(), 1);
+
+    const matrix::Matrix<int,0> czdm4 = 8;
+    matrix::Matrix<int, 0, 44> zdm4 = czdm4;
+    ASSERT_EQ(zdm4, 8);
+    zdm4 = 9;
+    ASSERT_EQ(zdm4, 9);
+    ASSERT_EQ(czdm4, 8);
+
+    const matrix::Matrix<int,0> zdm5 = czdm4;
+    ASSERT_EQ(zdm5, static_cast<int>(czdm4) );
+
+    matrix::Matrix<int,0, 800> m1;
+    matrix::Matrix<int,0> m2 = m1;
+    ASSERT_EQ(m2, 800);
+
+    matrix::Matrix<char,0> m3;
+    m3 = 'a';
+    ASSERT_EQ(m3,'a');
 }
 
 TEST(MATRIX_TEST, one_dimensional_matrix_test)
@@ -38,4 +62,40 @@ TEST(MATRIX_TEST, one_dimensional_matrix_test)
     zdm = 1;
     ASSERT_EQ(zdm, 1);
     ASSERT_EQ(odm[1], 5);
+
+    ASSERT_EQ(odm.size(), 1);
+
+    odm[2] = 4;
+    ASSERT_EQ(odm.size(), 2);
+    ASSERT_EQ(odm[2], 4);
+    ASSERT_EQ(odm[0], 0);
+
+    for(int i = 0; i < 10000; ++i)
+    {
+        if(i == 1)
+            ASSERT_EQ(odm[i], 5);
+        else if(i == 2)
+            ASSERT_EQ(odm[i], 4);
+        else
+            ASSERT_EQ(odm[i], 0);
+    }
+
+}
+
+TEST(MATRIX_TEST, two_dimensional_test)
+{
+    matrix::Matrix<int,2,-1> matrix;
+    ASSERT_EQ(matrix.size(), 0);
+
+    auto a = matrix[0][0];
+    ASSERT_EQ(a,-1);
+    ASSERT_EQ(matrix.size(), 0);
+
+    matrix[100][100] = 314;
+    ASSERT_EQ(matrix[100][100], 314);
+    ASSERT_EQ(matrix.size(), 1);
+
+    ((matrix[100][100] = 2)=4)=8;
+    ASSERT_EQ(matrix[100][100], 8);
+    ASSERT_EQ(matrix.size(), 1);
 }
